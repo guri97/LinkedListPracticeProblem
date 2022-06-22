cfefd8585a0edcae6bacd231fd8b48b245742407
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-22 21:30:10 +0530

    [gurpreet] UC1_SimpleLinkedList

---
+++ LinkedListPracticeProblem/LinkedListPracticeProblem/LinkedList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_LinkedList
+{
+    public class LinkedLists
+    {
+        public Node Head;
+        public Node Tail;
+
+        public LinkedLists()
+        {
+            Head = null;
+            Tail = null;
+        }
+
+        internal void Display()
+        {
+
+            Node temp = Head;
+            if (temp == null)
+                Console.WriteLine("Linked list is empty");
+            else
+                Console.WriteLine("Linked List:");
+            while (temp != null)
+            {
+                Console.Write(temp.data);
+                if (temp.next != null)
+                    Console.Write(" -> ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ LinkedListPracticeProblem/LinkedListPracticeProblem/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Day14_LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedLists linkedLists = new LinkedLists();
+
+            //UC-1
+            Node node1 = new Node(56);
+            Node node2 = new Node(30);
+            Node node3 = new Node(70);
+            node1.next = node2;
+            node2.next = node3;
+            linkedLists.Head = node1;
+            linkedLists.Display();
+            //uc1 o/p:Linked List:56-> 30-> 70
+
+
+        }
+    }
+}
